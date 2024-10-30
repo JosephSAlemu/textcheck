765adbc0da8f18786676cd2e72100c14ac035215
JosephSAlemu <jojoalemu@gmail.com>
2024-10-30 15:57:35 -0500

    removed cache

--- target/debug/deps/lazy_static-380a978cd56e0f88.d
+++
@@ -1,6 +0,0 @@
-/home/jojoa/RustP/RustTextCheck/textcheck/target/debug/deps/liblazy_static-380a978cd56e0f88.rmeta: /home/jojoa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.5.0/src/lib.rs /home/jojoa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.5.0/src/inline_lazy.rs
-
-/home/jojoa/RustP/RustTextCheck/textcheck/target/debug/deps/lazy_static-380a978cd56e0f88.d: /home/jojoa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.5.0/src/lib.rs /home/jojoa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.5.0/src/inline_lazy.rs
-
-/home/jojoa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.5.0/src/lib.rs:
-/home/jojoa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.5.0/src/inline_lazy.rs:
--- target/debug/textcheck.d
+++
@@ -1,1 +0,0 @@
-/home/jojoa/RustP/RustTextCheck/textcheck/target/debug/textcheck: /home/jojoa/RustP/RustTextCheck/textcheck/src/main.rs
